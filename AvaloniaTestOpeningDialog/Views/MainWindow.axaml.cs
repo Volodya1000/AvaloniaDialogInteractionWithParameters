fb408abcb2e2426a48238dc6e6d88e83dbdf266d
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-06-26 14:47:25 +0300

    /add comments

--- AvaloniaTestOpeningDialog/Views/MainWindow.axaml.cs
+++ AvaloniaTestOpeningDialog/Views/MainWindow.axaml.cs
@@ -15,19 +15,28 @@ public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
         
         ViewModel = viewModel;
 
+        // Добавление реактивных биндингов при активации окна
         this.WhenActivated(disposables =>
         {
+            // Привязка команды OpenDialogCommand из ViewModel к кнопке OpenDialogButton в UI
             this.BindCommand(ViewModel, vm => vm.OpenDialogCommand, v => v.OpenDialogButton)
-                .DisposeWith(disposables);
+                .DisposeWith(disposables); // Автоматическое удаление привязки при деактивации
 
             ViewModel.ShowDialogInteraction.RegisterHandler(async interaction =>
             {
+                // Создаем диалоговое окно через фабрику
                 var dialog = dialogFactory();
+
+                // Получаем ViewModel диалога и инициализируем ее входными данными
                 var dialogVm = dialog.DataContext as MyDialogViewModel;
                 dialogVm!.Init(interaction.Input);
+
+                // Показываем диалог модально и ждем результат
                 var res = await dialog.ShowDialog<MyDialogResult?>(this);
+
+                // Передаем результат обратно в interaction
                 interaction.SetOutput(res);
-            }).DisposeWith(disposables);
+            }).DisposeWith(disposables);// Автоматическая отписка при деактивации
         });
     }
 
