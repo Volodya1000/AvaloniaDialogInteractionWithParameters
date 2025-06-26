d2a1f4140ed4d80becf5a67bd7ab141b15e94227
volodya.dichkovski <volodya.dichkovski@gmail.com>
2025-06-26 16:20:33 +0300

    /save

--- AvaloniaTestOpeningDialog/App.axaml.cs
+++ AvaloniaTestOpeningDialog/App.axaml.cs
@@ -3,9 +3,6 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AvaloniaTestOpeningDialog.Views;
 using Microsoft.Extensions.DependencyInjection;
-using Model;
-using System;
-using ViewModels;
 
 namespace AvaloniaTestOpeningDialog;
 
@@ -18,30 +15,9 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
-        var services = new ServiceCollection();
-        services.AddTransient<MainWindowViewModel>();
-        services.AddTransient<MainWindow>();
-
-
-        services.AddTransient<MyDialogWindow>(sp =>
-            new MyDialogWindow(sp.GetRequiredService<MyDialogViewModel>()));
-
-        services.AddTransient<Func<MyDialogParams, MyDialogViewModel>>(provider =>
-            param => new MyDialogViewModel(param));
-
-        services.AddTransient<Func<MyDialogParams, MyDialogWindow>>(provider =>
-            param =>
-            {
-                var vmFactory = provider.GetRequiredService<Func<MyDialogParams, MyDialogViewModel>>();
-                var vm = vmFactory(param);
-                return new MyDialogWindow(vm);
-            });
-
-        var provider = services.BuildServiceProvider();
-
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = provider.GetRequiredService<MainWindow>();
+            desktop.MainWindow = Program.ServiceProvider.GetRequiredService<MainWindow>();
         }
 
         base.OnFrameworkInitializationCompleted();
--- AvaloniaTestOpeningDialog/Program.cs
+++ AvaloniaTestOpeningDialog/Program.cs
@@ -1,24 +1,52 @@
-﻿using System;
-using Avalonia;
+﻿using Avalonia;
 using Avalonia.ReactiveUI;
+using AvaloniaTestOpeningDialog.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Model;
+using System;
+using ViewModels;
 
-namespace AvaloniaTestOpeningDialog
+namespace AvaloniaTestOpeningDialog;
+
+internal sealed class Program
 {
-    internal sealed class Program
-    {
-        // Initialization code. Don't use any Avalonia, third-party APIs or any
-        // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-        // yet and stuff might break.
-        [STAThread]
-        public static void Main(string[] args) => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    public static IServiceProvider ServiceProvider { get; private set; }
 
-        // Avalonia configuration, don't remove; also used by visual designer.
-        public static AppBuilder BuildAvaloniaApp()
-            => AppBuilder.Configure<App>()
-                .UsePlatformDetect()
-                .WithInterFont()
-                .LogToTrace()
-                .UseReactiveUI();
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        ServiceProvider = services.BuildServiceProvider();
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
     }
+
+    private static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<MainWindowViewModel>();
+        services.AddTransient<MainWindow>();
+
+        services.AddTransient<MyDialogWindow>(sp =>
+            new MyDialogWindow(sp.GetRequiredService<MyDialogViewModel>()));
+
+        services.AddTransient<Func<MyDialogParams, MyDialogViewModel>>(provider =>
+            param => new MyDialogViewModel(param));
+
+        services.AddTransient<Func<MyDialogParams, MyDialogWindow>>(provider =>
+            param =>
+            {
+                var vmFactory = provider.GetRequiredService<Func<MyDialogParams, MyDialogViewModel>>();
+                var vm = vmFactory(param);
+                return new MyDialogWindow(vm);
+            });
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace()
+            .UseReactiveUI();
 }
